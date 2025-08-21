b6ee63511e26c7c73ee82b4cbc53c05773fc4040
Sam Crowder <samecrowder@gmail.com>
2025-08-20 17:21:46 -0700

    had to upgrade mcp version....can at least communicate now

--- examples/mcp/client.py
+++ examples/mcp/client.py
@@ -1,24 +1,24 @@
-"""MCP client to test talking with the MCP app in the langchain-tools-server"""
+"""MCP client to test talking with the MCP app using streamable HTTP"""
 
 import asyncio
 
 from mcp import ClientSession
-from mcp.client.sse import sse_client
+import mcp.client.streamable_http as streamable_http
 
 
 async def main():
     if len(sys.argv) < 2:
         print(
-            "Usage: uv run client.py <URL of SSE MCP server (i.e. http://localhost:8080/mcp/sse)>"
+            "Usage: uv run client.py <URL of HTTP MCP server (i.e. http://localhost:8002/mcp)>"
         )
         sys.exit(1)
 
     url = sys.argv[1]
 
-    if "mcp" not in url and "sse" not in url:
-        raise ValueError("Use url format of [host]/mcp/sse")
+    if "mcp" not in url:
+        raise ValueError("Use url format of [host]/mcp")
 
-    async with sse_client(url=url) as streams:
+    async with streamable_http.streamablehttp_client(url=url) as streams:
         async with ClientSession(streams[0], streams[1]) as session:
             await session.initialize()
             tools = await session.list_tools()
