a43279887f860a5684b095d65c82094222b11625
Sam Crowder <samecrowder@gmail.com>
2025-08-20 17:07:43 -0700

    one shot attempt at rewriting sse to streamable http

--- libs/server/universal_tool_server/__init__.py
+++ libs/server/universal_tool_server/__init__.py
@@ -66,9 +66,10 @@ class Server:
         self._enable_mcp = enable_mcp
 
         if enable_mcp:
-            from universal_tool_server.mcp import MCP_APP_PREFIX, create_mcp_app
+            from universal_tool_server.mcp import create_mcp_router
 
-            self.app.mount(MCP_APP_PREFIX, create_mcp_app(self.tool_handler))
+            mcp_router = create_mcp_router(self.tool_handler)
+            self.app.include_router(mcp_router, prefix="/mcp")
 
     @overload
     def add_tool(
